afd41c076c7c3f14e6797674d7a62e79ecd0a62f
Yuri Yakovlev <contact@yakovlevyuri.com>
2017-05-22 18:14:37 +0200

    project stucture modernization

--- gulpfile.js
+++ gulpfile.js
@@ -2,51 +2,39 @@ var gulp = require('gulp');
 var path = require('path');
 var uglify = require('gulp-uglify');
 var gp_concat = require('gulp-concat');
-var sass = require('gulp-sass');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 
 var jsFiles = [
   'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/slick-carousel/slick/slick.min.js',
-  'scripts/main.js'
+  'node_modules/highlightjs/highlight.pack.js',
+  'node_modules/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.min.js',
+  'node_modules/instafeed.js/instafeed.min.js',
+  'src/scripts.js'
 ];
 
-gulp.task('default', ['js', 'sass', 'watch']);
+gulp.task('default', ['js', 'less', 'watch']);
+gulp.task('build', ['js', 'less']);
 
 gulp.task('js', function () {
   gulp.src(jsFiles).pipe(sourcemaps.init())
     .pipe(gp_concat('scripts.min.js'))
     .pipe(sourcemaps.write('../js'))
-    .pipe(gulp.dest('develop/js'));
-});
-
-gulp.task('sass', function () {
-  return gulp.src('styles/front.scss')
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('./develop/css'));
+    .pipe(gulp.dest('public/static/js'));
 });
 
 gulp.task('less', function () {
-  return gulp.src('less/style.less')
+  return gulp.src('src/assets/styles/front.less')
+    .pipe(sourcemaps.init())
+    .pipe(gp_concat('front.min.css'))
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ],
       compress: true
     }))
-    .pipe(gulp.dest('./production/css'));
+    .pipe(gulp.dest('public/static/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('styles/**/*.scss', ['sass']);
+  gulp.watch('src/assets/styles/**/*.less', ['less']);
   gulp.watch(jsFiles, ['js']);
-});
-
-gulp.task('production', function() {
-  gulp.src(jsFiles).pipe(gp_concat('scripts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('production/js'));
-
-  gulp.src('styles/front.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./production/css'));
 });
