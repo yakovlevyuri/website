f8080bc9751f141899b9fc90ca6d1323b0675ec7
Yuri Yakovlev <contact@yakovlevyuri.com>
2018-06-28 15:28:09 +0200

    Add new page for my lovely HRs

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,10 @@
 
 import React from 'react';
 
-import Layout from '../src/components/Layout';
 import Info from '../src/components/info/Info';
-/*import Editor from '../src/components/editor/Editor';*/
 
 const IndexPage = () => (
-  <Layout>
-    <Info name="Yuri Yakovlev" position="Full Stack Javascript Developer" />
-    {/*<Editor />*/}
-
-    <style jsx>{`
-      .home2 {
-        top: -60px;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        overflow: hidden;
-        // position: absolute;
-        align-items: center;
-        justify-content: center;
-      }
-
-      .wrapper1 {
-        max-width: 70%;
-        align-self: center;
-        text-align: center;
-      }
-    `}</style>
-  </Layout>
+  <Info name="Yuri Yakovlev" position="Full Stack Javascript Developer" />
 );
 
 export default IndexPage;
---
+++ pages/lets-play-a-game.js
@@ -0,0 +1,116 @@
+// @flow
+
+import React, { Component } from 'react';
+
+import Link from '../src/components/link/Link';
+import Ul, { LI } from '../src/components/post/NumbersList';
+import { logEvent } from '../src/utils/analytics';
+
+class GamePage extends Component<null> {
+  render() {
+    return (
+      <div>
+        <p>
+          Welcome here! My name is Yuri and I’m a Fullstack JavaScript
+          Developer.
+        </p>
+        <p>
+          Probably you were trying to offer me an interesting job opportunity
+          and most likely I redirected you to this page.
+        </p>
+        <p>
+          First of all I would like to thank you for your interest in me as a
+          candidate, I appreciate that, I really do. Secondly I respect your
+          time as much as I respect mine so please let’s spend it as productive
+          as possible.
+        </p>
+        <p>
+          I get literally tons of HR messages on{' '}
+          <Link
+            url="https://cz.linkedin.com/in/yakovlevyuri"
+            title="LinkedIn"
+            label="LinkedIn"
+            newWindow
+            onClick={() =>
+              logEvent('Link', 'Clicked', 'LinkedIn from GamePage')
+            }
+          />{' '}
+          and 99% of them are irrelevant. So I came up with the idea how to
+          avoid that.
+        </p>
+        <p>
+          <strong>
+            All I ask you to do is to follow only 4 criteria before you’re going
+            to offer me a new role:
+          </strong>
+        </p>
+        <Ul>
+          <LI>
+            I’m crazy in ❤️ with <em>JavaScript</em> and I’m not going to be a
+            Java/C#/Python/whatsoever hero for you or someone else.
+          </LI>
+          <LI>
+            Still using RESTful API? No God, please no. I prefer{' '}
+            <em>GraphQL + Node.js</em> bundle.
+          </LI>
+          <LI>
+            What about client side? I like <em>React</em> and <em>Vue</em>.
+          </LI>
+          <LI>
+            I am also a huge fan of <em>Open Source</em> and I expect from my
+            future employer to share the same views as well.
+          </LI>
+        </Ul>
+        <p>
+          Checked every point above carefully and pretty sure that{' '}
+          <em>I’m the guy</em> and <em>you are that 1%</em>? Please drop me an{' '}
+          <Link
+            url="mailto:contact@yakovlevyuri.com"
+            title="Drop me an email"
+            label="email"
+            newWindow
+            onClick={() =>
+              logEvent('Link', 'Clicked', 'Contact via email from GamePage')
+            }
+          />{' '}
+          or{' '}
+          <Link
+            url="https://t.me/yyakovlev"
+            title="I use telegram"
+            label="telegram message"
+            newWindow
+            onClick={() =>
+              logEvent('Link', 'Clicked', 'Contact via telegram from GamePage')
+            }
+          />
+        </p>
+        <style jsx global>{`
+          .wrapper {
+            @media screen and (min-width: 820px) {
+              grid-template-columns: 1fr 550px 1fr;
+              grid-template-areas:
+                'header header header'
+                '. content .'
+                'footer footer footer' !important;
+            }
+          }
+
+          .content {
+            text-align: left !important;
+          }
+
+          ul {
+            margin-left: 20px !important;
+          }
+        `}</style>
+        <style jsx>{`
+          p {
+            padding-bottom: 20px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default GamePage;
